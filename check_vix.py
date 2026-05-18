f852899d35e8ea842fa6f5ab0f9641f61e2a82bf
weichelin72-ctrl <weichelin72@gmail.com>
2026-05-18 17:00:06 +0800

    Add files via upload

---
+++ check_vix.py
@@ -0,0 +1,26 @@
+import os
+import requests
+import yfinance as yf
+
+THRESHOLD = 25
+
+BOT_TOKEN = os.environ["BOT_TOKEN"]
+CHAT_ID = os.environ["CHAT_ID"]
+
+def send_telegram(msg):
+    url = f"https://api.telegram.org/bot{BOT_TOKEN}/sendMessage"
+
+    payload = {
+        "chat_id": CHAT_ID,
+        "text": msg
+    }
+
+    requests.post(url, json=payload)
+
+vix = yf.Ticker("^VIX")
+price = vix.history(period="1d").Close.iloc[-1]
+
+print("Current VIX:", price)
+
+if price > THRESHOLD:
+    send_telegram(f"?? VIX ¶W¹L {THRESHOLD}\n¥Ø«e VIX: {price:.2f}")
