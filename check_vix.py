17397db4b40e4bdca4f5b0ac8c3349601adb570a
weichelin72-ctrl <weichelin72@gmail.com>
2026-05-18 17:22:44 +0800

    Update check_vix.py

--- check_vix.py
+++ check_vix.py
@@ -2,7 +2,14 @@ import os
 import requests
 import yfinance as yf
 
-THRESHOLD = 20
+
+# real threshold
+VIX_THRESHOLD = 20
+ETF_THRESHOLD = 85
+# testing only
+VIX_THRESHOLD = 1
+ETF_THRESHOLD = 200
+
 
 BOT_TOKEN = os.environ["BOT_TOKEN"]
 CHAT_ID = os.environ["CHAT_ID"]
@@ -17,10 +24,24 @@ def send_telegram(msg):
 
     requests.post(url, json=payload)
 
+# ===== VIX =====
 vix = yf.Ticker("^VIX")
-price = vix.history(period="1d").Close.iloc[-1]
+vix_price = vix.history(period="1d").Close.iloc[-1]
 
-print("Current VIX:", price)
+print("VIX:", vix_price)
 
-if price > THRESHOLD:
-    send_telegram(f"⚠️ VIX 超過 {THRESHOLD}\n目前 VIX: {price:.2f}")
+if vix_price > VIX_THRESHOLD:
+    send_telegram(
+        f"VIX 超過 {VIX_THRESHOLD}\n目前 VIX: {vix_price:.2f}"
+    )
+
+# ===== 0050 =====
+etf = yf.Ticker("0050.TW")
+etf_price = etf.history(period="1d").Close.iloc[-1]
+
+print("0050:", etf_price)
+
+if etf_price < ETF_THRESHOLD:
+    send_telegram(
+        f"0050 低於 {ETF_THRESHOLD}\n目前 0050: {etf_price:.2f}"
+    )
