17827b04b65ca1a26bc374c6aaaed6bfaf6f3b62
weichelin72-ctrl <weichelin72@gmail.com>
2026-05-19 10:29:14 +0800

    Update check_vix.py

--- check_vix.py
+++ check_vix.py
@@ -6,9 +6,9 @@ import yfinance as yf
 # real threshold
 VIX_THRESHOLD = 20
 ETF_THRESHOLD = 85
-# testing only
-VIX_THRESHOLD = 1
-ETF_THRESHOLD = 200
+# # testing only
+# VIX_THRESHOLD = 1
+# ETF_THRESHOLD = 200
 
 
 BOT_TOKEN = os.environ["BOT_TOKEN"]
