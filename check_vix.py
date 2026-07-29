24b614816678a0564916f16f2853b5e8fa1f9153
weichelin72-ctrl <weichelin72@gmail.com>
2026-07-29 09:41:31 +0800

    Update check_vix.py

--- check_vix.py
+++ check_vix.py
@@ -57,7 +57,7 @@ if etf_price < ETF_THRESHOLD:
 # ===== Testing =====
 send_telegram(
 #    f"TEST\nVIX: {vix_price:.2f}\n0050: {etf_price:.2f}"
-    f"TEST VIX:{vix_price:.2f}, 0050:{etf_price:.2f}"
+    f"TEST:  VIX={vix_price:.2f}; 0050={etf_price:.2f}"
 )
 
 # ===== crossing alert =====
